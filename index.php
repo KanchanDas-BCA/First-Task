1906448db6b256b8c425b473e14cfadb204a5ae9
Utshav Dahal <utshav.dahalk99@gmail.com>
2025-05-05 12:11:03 +0545

    added text

--- index.php
+++ index.php
@@ -3,5 +3,6 @@ echo '<h1>Welcome to Bardiya !</h1>';
 echo '<h1>Welcome to Biratnagar !</h1>';
 echo '<h1>Welcome to Janakpur !</h1>';
 echo'<img src="babai.jpeg"width="1024px">';
+echo '<h1>Welcome to Biratnagar !</h1>';
 echo'<img src="brt.png"width="1024px">';
 ?>
