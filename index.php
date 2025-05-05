78bf8287317a01748a60884cbaa6c711662875fb
Utshav Dahal <utshav.dahalk99@gmail.com>
2025-05-05 11:58:06 +0545

    2nd commit

--- index.php
+++ index.php
@@ -1,6 +1,7 @@
 <?php
 echo '<h1>Welcome to Bardiya !</h1>';
 echo '<h1>Welcome to Biratnagar !</h1>';
+echo '<h1>Welcome to Janakpur !</h1>';
 echo'<img src="babai.jpeg"width="1024px">';
 echo'<img src="brt.png"width="1024px">';
 ?>
